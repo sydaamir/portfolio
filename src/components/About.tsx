d4870156251a1b704c108844c01e342c709f4bc5
syed aamir nazir <syedaamirnazir@syeds-MacBook-Air.local>
2024-08-12 20:02:58 +0530

    fixed Apostrophes bug

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,11 +7,11 @@ const About: React.FC = () => {
         <h2 className="text-3xl font-bold mb-8 text-teal-600">About Me</h2>
         <p className="text-lg mb-4 text-gray-700">
         I am a skilled React UI Developer with over 5 years of experience in creating dynamic and user-friendly web applications. 
-        My expertise spans front-end development with React, complemented by a strong foundation in Java and Spring MVC, which I've specialized in over the past two years. 
+        My expertise spans front-end development with React, complemented by a strong foundation in Java and Spring MVC, which I&apos;ve specialized in over the past two years. 
         
         </p>
         <p className="text-lg text-gray-700">
-        With a Master's degree in Computer Applications (MCA), I bring a deep understanding of software development principles,
+        With a Master&apos;s degree in Computer Applications (MCA), I bring a deep understanding of software development principles,
         enabling me to build seamless, efficient, and scalable applications. I am dedicated to writing clean, maintainable code and continuously exploring new technologies to deliver high-quality solutions.
         </p>
       </div>
